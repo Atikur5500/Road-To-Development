1d525392643c278ec4078ef062c96bacccb34a0e
Atikur R Shanto <atikur.r.shanto@gmail.com>
2023-09-06 08:53:15 +0600

    Complete Milestone 6

---
+++ Milestone-06/M-36.5 Browser & Debugging/01.prompt.js
@@ -0,0 +1,26 @@
+// alert
+const moreAlert = () =>{
+    alert('Again welcome to chrome browser')
+}
+
+// Confirm
+const seriousStudent = () =>{
+    const serious = confirm('Are you serious about your mission?')
+    console.log(serious)
+    if (serious === true) {
+        alert('Yes! You are the consistent about your mission!')
+    } else {
+        console.log('Please be serious!')
+    }
+}
+
+// Prompt
+
+const getName = () =>{
+    const getName = prompt('Write Your Name?');
+    console.log(getName)
+}
+
+
+
+
